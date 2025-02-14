fce4f0401dea53a63f4d8b5a3ae5f4d279cf6e60
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-02-14 16:08:45 +0530

    Screen_Share

--- Frontend/src/BrowseSkill/BrowseSkill.jsx
+++ Frontend/src/BrowseSkill/BrowseSkill.jsx
@@ -59,7 +59,7 @@ function Browseskill() {
 
     async function moveToProfile(id) {
         const timeStamp = Date.now()
-        const response = await fetch(`http://localhost:3000/profileview/${id}`, {
+        const response = await fetch(`${backendUrl}/profileview/${id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": 'application/json'
