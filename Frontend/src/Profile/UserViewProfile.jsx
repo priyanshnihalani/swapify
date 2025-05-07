d450fb2dd71d8ab9feb885e61669ce2e98b89718
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-05-07 21:53:39 +0530

    Just Change

--- Frontend/src/Profile/UserViewProfile.jsx
+++ Frontend/src/Profile/UserViewProfile.jsx
@@ -144,7 +144,6 @@ function UserViewProfile() {
                 const response = await fetch(`${backendUrl}/uploadCover/${data._id}`, {
                     method: "PATCH",
                     headers: {
-                        'Content-Type': FormData,
                         'Authorization': `Bearer ${token}`
                     },
                     body: formData,
