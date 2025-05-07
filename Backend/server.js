6690592331ca935f2e87b81f29633dd22c20bced
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-05-07 22:04:42 +0530

    CORS

--- Backend/server.js
+++ Backend/server.js
@@ -29,10 +29,7 @@ const app = express();
 app.use(cors({
     origin: "https://swapiify.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true
 }));
-
-app.options('*', cors())
 
 app.use(express.json());
 
